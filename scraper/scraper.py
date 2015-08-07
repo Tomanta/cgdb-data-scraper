0283d2bdcea5771d01be160434a8f6c98c80cbe2
Zed <bzednick+git@gmail.com>
2015-08-06 23:57:36 -0500

    Started framework for processing the list sections
    Started framework for processing the list sections

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -16,7 +16,26 @@ soup = BeautifulSoup(r.content, 'html.parser')
 # As a starting point, this extracts the card image and the card name.
 # TODO: Maybe change this to a MAP call?
 for tag in soup.findAll('div', {'class': "cardRecord"}):
+  card = {} # start a new card dictionary
+
+  # For the first part let's grab the things every card will have that's NOT in the
+  # <ul> section
+
   tag.find('div', {'class': 'cardImage'}).find('img')['src']
   tag.find('div', {'class': 'cardText'}).find('h1').text
-  break  # TEMP: Let's get this down for the first record before worrying about the rest
+  
+  # Now let's add things from the list
+  for attribute in tag.findAll('li'):
     
+    # a few items have class, that makes it easy!
+    if attribute.has_attr('class') and len(attribute.text) > 0:
+       attribute.text
+       if 'traits' in attribute['class']:
+           card['traits'] = attribute.text
+       elif 'flavorText' in attribute['class']:
+           card['flavorText'] = attribute.text
+    # There is a <SPAN> object for the card text, most of the rest are just X: Y text.         
+    break # TEMP: Just to speed things up instead of processing ALL the cards.
+
+
+
